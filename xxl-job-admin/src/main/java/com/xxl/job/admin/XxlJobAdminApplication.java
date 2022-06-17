33455da476284383c712e20a9ef174b9758f482a
刘纯林 <liuchunlin@cennavi.com.cn>
2022-06-17 20:16:05 +0800

    支持nacos

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class XxlJobAdminApplication {
 
